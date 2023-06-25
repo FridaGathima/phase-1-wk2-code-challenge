6f04e412e94e18999eb729bdfc72cbbe9d603c29
+ <+>
2023-06-26 01:09:05 +0300

    js click added

--- index.js
+++ index.js
@@ -4,25 +4,34 @@ let getAnimals = fetch("http://localhost:3000/characters") // I used fetch() to 
         let ul = document.querySelector("ul")  // The names of the animals will be under ul 
         data.forEach((characters) => {              //I looped through the data and out of it picked each character 
             const li = document.createElement("li") // This is to create li for every character instead of doing it in HTML
-            li.textContent = characters.name  // These are the names of the animals that will be displayed in the webpage
-            ul.appendChild(li)
-        })
+        li.textContent = characters.name  // These are the names of the animals that will be displayed in the webpage
+        ul.appendChild(li)
     })
+})
 
+
+let getId = fetch("http://localhost:3000/characters") // I used fetch() to get the data from the JSON server to javascript
+    .then ((response) => response.json()) 
+    .then ((data) => {
+        let ul = document.querySelector("ul")  // The ids of the animals will be under ul 
+        data.forEach((characters) => {              //I looped through the data and out of it picked each character 
+            const li = document.createElement("li") // This is to create li for every character instead of doing it in HTML
+        li.textContent = characters.id // These are the ids of the animals that will be displayed in the webpage
+        ul.appendChild(li)
+    })
+})
 
 function description (){
-    let more = document.createElement("div")
-    more.className = "moreDescription"
-    more.innerText = "characters.id"
+    let more = document.createElement('div')
+    more.className = 'moreDescription'
+    more.innerText = 'testing'
     animals.appendChild(more)
 }
-//description()
-
 
 
 // On clicking
-//const ul = document.querySelector("ul")
-//ul.addEventListener("click", (e) =>{
-//console.log ("click")
-//})
+const ul = document.querySelector("ul")
+ul.addEventListener("click", (e) =>{
+console.log ("click")
+})
 
